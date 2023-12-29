c8d0d7bdbba686c69efa9e21df09c7c2a20cdbe5
Jehiah Czebotar <jehiah@gmail.com>
2023-12-28 22:31:31 -0500

    date updates

--- query_intersections.sql
+++ query_intersections.sql
@@ -11,11 +11,11 @@ END as county,
   ARRAY_AGG(distinct regexp_replace(LOWER(intersecting_street), "((^[^/]+$)|(.+ (.+/of .*)))", "\\2\\4") IGNORE NULLS ) as intersections,
   ARRAY_AGG(distinct issuing_agency) as agencies,
   count(*) as number_violations
-FROM `jehiah-p1.opendata.parking_violations_fy_2023_pvqr_7yc4`
+FROM `jehiah-p1.opendata.parking_violations_fy`
 WHERE 
 issuing_agency in ('T','P','S')
 and issue_date is not null
-and issue_date between '2022-07-01' and CURRENT_DATE
+and issue_date between '2022-10-01' and CURRENT_DATE
 and street_name is not null
 and violation_county is not null
 and violation_county in ("MN", "NY", "BX", "Bronx", "BK", "K", "Kings", "Q", "QN", "Qns", "R", "Rich", "ST")
