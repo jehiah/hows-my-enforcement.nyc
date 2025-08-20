0b9f272094612a4ea0fefcc319f92f561a0504a4
Jehiah Czebotar <jehiah@gmail.com>
2025-08-20 08:44:24 -0400

    data update

--- update_summary.sh
+++ update_summary.sh
@@ -3,7 +3,7 @@ echo "querying FY 2023-2024 stats"
 DATASET=pvqr-7yc4
 # curl 'https://data.cityofnewyork.us/resource/pvqr-7yc4.json?$select=issuing_agency,issue_date,count(*)+as+number_violations&$where=issuing_agency+in+('T','P','S')&$group=issuing_agency,issue_date&$having=number_violations+>+50&$limit=2000' > temp.json
 
-curl -s "https://data.cityofnewyork.us/resource/${DATASET}.json?\$select=issuing_agency,issue_date,count(*)+as+number_violations&\$where=issuing_agency+in+(%27T%27,%27P%27,%27S%27)&\$group=issuing_agency,issue_date&\$having=number_violations+%3E+50&\$limit=2000" > temp.json || echo "error" >&2
+curl -s "https://data.cityofnewyork.us/resource/${DATASET}.json?\$select=issuing_agency,issue_date,count(*)+as+number_violations&\$where=issuing_agency+in+(%27T%27,%27P%27,%27S%27)&\$group=issuing_agency,issue_date&\$having=number_violations+%3E+50&\$limit=5000" > temp.json || echo "error" >&2
 
 # https://data.cityofnewyork.us/resource/7mxj-7a6y.json?$select=issuing_agency,issue_date,count(*)+as+number_violations&$where=issuing_agency+in+('T','P','S','K')&$group=issuing_agency,issue_date&$having=number_violations+>+50&$limit=2000
 
