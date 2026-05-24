dd26fa6726bf9ee09292b50057c5f29fad9f253e
Jehiah Czebotar <jehiah@gmail.com>
2026-05-24 13:45:14 -0400

    refactor - extract shared consts to data.js

--- www/fy_lookup.js
+++ www/fy_lookup.js
@@ -3,39 +3,39 @@ function parseDate(dateString) {
 }
 
 export const FYLookup = {
-        "FY2026": {
+    "FY2026": {
         "dataset": "pvqr-7yc4",
         "start": parseDate("2025-07-01"),
         "end": {
-            "P":parseDate("2026-01-14"),
-            "T":parseDate("2026-02-24"),
-            "S":parseDate("2026-02-25")
+            "P": parseDate("2026-01-14"),
+            "T": parseDate("2026-02-24"),
+            "S": parseDate("2026-02-25")
         }
-        },
-        "FY2025": {
-        "dataset":"m5vz-tzqv", 
+    },
+    "FY2025": {
+        "dataset": "m5vz-tzqv",
         "start": parseDate("2024-07-01"),
         "end": parseDate("2025-07-01"),
-        },
-        "FY2024": {
-        "dataset":"8zf9-spf8", 
+    },
+    "FY2024": {
+        "dataset": "8zf9-spf8",
         "start": parseDate("2023-07-01"),
         "end": parseDate("2024-07-01"),
-        },
-        "FY2023": {
-        "dataset":"869v-vr48", 
+    },
+    "FY2023": {
+        "dataset": "869v-vr48",
         "start": parseDate("2022-07-01"),
         "end": parseDate("2023-06-30")
-        },
-        "FY2022": {
-        "dataset":"pvqr-7yc4",
+    },
+    "FY2022": {
+        "dataset": "pvqr-7yc4",
         "start": parseDate("2022-07-01"),
         "end": parseDate("2023-07-01")
-        },
-        "FY2021": {
-        "dataset":"7mxj-7a6y",
+    },
+    "FY2021": {
+        "dataset": "7mxj-7a6y",
         "start": parseDate("2021-07-01"),
         "end": parseDate("2022-06-30")
-        }
     }
+}
     
